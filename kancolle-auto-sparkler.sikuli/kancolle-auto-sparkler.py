71f7101396c52206eff2acc13c4c98c66d0962e5
0x384c0 <0x384c0@gmail.com>
2017-08-13 21:56:56 +0600

    added fleet selection

--- kancolle-auto-sparkler.sikuli/kancolle-auto-sparkler.py
+++ kancolle-auto-sparkler.sikuli/kancolle-auto-sparkler.py
@@ -1,4 +1,5 @@
 WAIT_TIMEOUT = 10
+FLEET_NUMBER = 4
 setAutoWaitTimeout(WAIT_TIMEOUT)
 def accept_battle_results():
     print("wait for battle finish")
@@ -20,6 +21,7 @@ def accept_battle_results():
     click("1499956703868.png")
     sleep(1)
     click("1499956703868.png")
+    waitVanish("1502635171297.png")
     # new ship
     if exists("1499956814235.png"):
         click("1499956814235.png")
@@ -30,9 +32,17 @@ hover("1499957259515.png")
 click("1499956459323.png")
 hover("1499957259515.png")
 
-click(Pattern("1499966616577.png").similar(0.90))
+click(Pattern("1502634102077.png").similar(0.95))
 click("1499956562272.png")
 hover("1499957259515.png")
+
+if FLEET_NUMBER == 2:
+    click("fleet_2.png")
+if FLEET_NUMBER == 3:
+    click("fleet_3.png")
+if FLEET_NUMBER == 4:
+    click("fleet_4.png")
+   
 click("1499956601222.png")
 
 accept_battle_results()
