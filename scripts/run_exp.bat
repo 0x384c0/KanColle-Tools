7a58b84682e398ef29cc2e465b277f16a0466c09
0x384c0 <0x384c0@gmail.com>
2019-01-18 10:05:17 +0600

    sleep after 10 min on crash in run_exp.bat

--- scripts/run_exp.bat
+++ scripts/run_exp.bat
@@ -1,2 +1,2 @@
-
-java -jar sikulix.jar -r ../kancolle-auto-expeditions.sikuli & minimise.vbs & "alarm.mp3"
+shutdown -a
+java -jar sikulix.jar -r ../kancolle-auto-expeditions.sikuli & minimise.vbs & shutdown -s -t 600 & "alarm.mp3"
